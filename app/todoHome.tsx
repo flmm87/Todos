d82c319a491e9b66300752a41e506ec5c4cca61f
Mino <minomurri@gmail.com>
2024-12-01 16:57:19 +0000

    Refractoring

--- app/todoHome.tsx
+++ app/todoHome.tsx
@@ -1,79 +1,12 @@
-import {
-  View,
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-} from "react-native";
-import React, { useEffect } from "react";
-import { useTodoContext } from "../store/todoContext";
-import TodosList from "../components/TodosList";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { useRouter } from "expo-router";
+import React from "react";
+import TodoHome from "../components/TodosHome";
 
 const todoHome = () => {
-  const { todoObject, setTodoObject } = useTodoContext();
-  const router = useRouter();
-
-  useEffect(() => {
-    const todoFromLocalStore = async () => {
-      try {
-        console.log("getting value");
-        const value = await AsyncStorage.getItem("todo");
-        if (value !== null) {
-          setTodoObject(JSON.parse(value));
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    todoFromLocalStore();
-  }, []);
-
   return (
     <>
-      <SafeAreaView />
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        {todoObject.length > 0 ? (
-          <View>
-            <TodosList data={todoObject} />
-          </View>
-        ) : (
-          <View style={styles.mainContainer}>
-            <Text style={styles.text}>Please add your first Todo</Text>
-            <View style={styles.iconContainer}>
-              <TouchableOpacity
-                style={{ padding: 10 }}
-                onPress={() => router.navigate("modalForm")}
-              >
-                <Icon name="plus" />
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
-      </View>
+      <TodoHome />
     </>
   );
 };
 
 export default todoHome;
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    justifyContent: "space-evenly",
-    alignItems: "center",
-  },
-
-  iconContainer: {
-    borderWidth: 2,
-    borderColor: "black",
-    borderRadius: 20,
-    flexDirection: "column",
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "400",
-  },
-});
---
+++ components/TodosHome.tsx
@@ -0,0 +1,78 @@
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect } from "react";
+import { useTodoContext } from "../store/todoContext";
+import TodosList from "./TodosList";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useRouter } from "expo-router";
+
+const TodoHome = () => {
+  const { todoObject, setTodoObject } = useTodoContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    const todoFromLocalStore = async () => {
+      try {
+        const value = await AsyncStorage.getItem("todo");
+        if (value !== null) {
+          setTodoObject(JSON.parse(value));
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    todoFromLocalStore();
+  }, []);
+
+  return (
+    <>
+      <SafeAreaView />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {todoObject.length > 0 ? (
+          <View>
+            <TodosList data={todoObject} />
+          </View>
+        ) : (
+          <View style={styles.mainContainer}>
+            <Text style={styles.text}>Please add your first Todo</Text>
+            <View style={styles.iconContainer}>
+              <TouchableOpacity
+                style={{ padding: 10 }}
+                onPress={() => router.navigate("modalForm")}
+              >
+                <Icon name="plus" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </View>
+    </>
+  );
+};
+
+export default TodoHome;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+
+  iconContainer: {
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 20,
+    flexDirection: "column",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "400",
+  },
+});
